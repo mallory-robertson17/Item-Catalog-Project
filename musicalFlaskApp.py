d592c4670b66cece9f8d9cd7094b777a4a8ae9cc
Mallory Robertson <mallory.robertson17@gmail.com>
2018-12-11 13:15:08 -0600

    add endpoint to create new actor

--- musicalFlaskApp.py
+++ musicalFlaskApp.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from musicalDBBuilder import Base, Actor, Musical, Character
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///broadwaymusicals.db')
+engine = create_engine('sqlite:///broadwaymusicals.db', connect_args={'check_same_thread':False})
 Base.metadata.bind = engine
+
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
@@ -16,7 +17,7 @@ def getMusical(musical_id):
     m = session.query(Musical).filter_by(id=musical_id).first()
     c = session.query(Character).filter_by(musical_id=m.id)
     print c
-    
+
     return render_template('musical.html', musical=m, characters=c)
 
 
@@ -45,6 +46,27 @@ def getActor(actor_id):
     a = session.query(Actor).filter_by(id=actor_id)
     c = session.query(Character).filter_by(actor_id=actor_id)
     return render_template('actor.html', actor=a, characters=c)
+
+
+@app.route('/actors/')
+def getAllActors():
+    actors = session.query(Actor)
+    message = ""
+    for x in actors:
+        message += "<html><body>" + x.name + "</body></html><br>"
+    print message
+    return message
+
+
+@app.route('/actors/new/', methods=['GET','POST'])
+def newActor():
+    if request.method == 'POST':
+        newActor = Actor(name = request.form['name'])
+        session.add(newActor)
+        session.commit()
+        return redirect(url_for('newActor'))
+    else:
+        return render_template('NewActor.html')
 
 
 if __name__ == '__main__':
