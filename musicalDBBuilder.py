a9507b68c72b542a6a7d9726d5bc67f6b0d78a82
Mallory Robertson <mallory.robertson17@gmail.com>
2018-12-27 14:12:45 -0600

    add user DB

--- musicalDBBuilder.py
+++ musicalDBBuilder.py
@@ -6,34 +6,44 @@ from sqlalchemy import create_engine
 
 Base= declarative_base()
 
-class Actor(Base):
-    __tablename__='actor'
-
+class User(Base):
+    __tablename__='user'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String(40), nullable=False)
-	createdBy = Column(String(40), nullable=False)
+    email = Column(String(40), nullable=False)
+	
+class Actor(Base):
+    __tablename__='actor'
+	
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
 
 class Musical(Base):
     __tablename__='musical'
-
+	
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     summary = Column(String(500))
     year = Column(String(8), nullable=False)
-	createdBy = Column(String(40), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-
-class Character(Base):
+	
+class Character(Base): 
     __tablename__='character'
-
+	
     id = Column(Integer, primary_key=True)
     name = Column(String(40), nullable=False)
     actor_id = Column(Integer, ForeignKey('actor.id'))
     actor = relationship(Actor)
     musical_id = Column(Integer, ForeignKey('musical.id'))
     musical = relationship(Musical)
-	createdBy = Column(String(40), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+#######insert at end of file#######
 
 engine = create_engine('sqlite:///broadwaymusicals.db')
 
